4219ad7c1861b14ec736c7374780f1d4e42d23d2
Fred <frederique.perrier@gmail.com>
2024-10-07 13:57:42 +0200

    ✨ Display oeuvre with bdd

--- oeuvre.php
+++ oeuvre.php
@@ -1,25 +1,20 @@
 <?php
+    require_once 'bddcnx.php';
     require 'header.php';
-    require 'oeuvres.php';
-
-    // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
-    if(empty($_GET['id'])) {
+    
+    if(empty($_GET['id'])){
         header('Location: index.php');
     }
 
     $oeuvre = null;
 
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
+    $query = $cnx->prepare('SELECT * FROM oeuvres WHERE id = ?');
+    $query->execute([$_GET['id']]);
+    $oeuvre = $query->fetch();
+
 
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
+    if(!$oeuvre) {
         header('Location: index.php');
     }
 ?>
